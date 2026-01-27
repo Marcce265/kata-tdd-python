42972d0b0eac613f427d1bc112ee30379196ac17
ESTUDIANTE HUANCAYO <estudiantehyo@continental.edu.pe>
2026-01-26 19:23:50 -0500

    Final: Casos n=0, n=1 y n=N funcionando

--- src/calculator.py
+++ src/calculator.py
@@ -1,12 +1,3 @@
-def standard_deviation(data):
-    if len(data) == 0:
-        return 0
-    
-
-def standard_deviation(data):
-    if len(data) <= 1: # Cambiamos == 0 por <= 1
-        return 0
-    
 import math
 
 def standard_deviation(data):
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,6 +3,9 @@ from src.calculator import standard_deviation
 def test_desviacion_vacia():
     assert standard_deviation([]) == 0
 
-
 def test_desviacion_un_dato():
-    assert standard_deviation([5]) == 0
+    assert standard_deviation([10]) == 0
+
+def test_desviacion_varios_datos():
+    # Desviación de 4 y 8 es 2.828
+    assert standard_deviation([4, 8]) == 2.828
