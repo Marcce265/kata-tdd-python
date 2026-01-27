5c580258fc527d3ef64dbd30e684b339da448c6d
ESTUDIANTE HUANCAYO <estudiantehyo@continental.edu.pe>
2026-01-26 19:20:02 -0500

    Test: caso n=N (Verde)

--- src/calculator.py
+++ src/calculator.py
@@ -6,3 +6,14 @@ def standard_deviation(data):
 def standard_deviation(data):
     if len(data) <= 1: # Cambiamos == 0 por <= 1
         return 0
+    
+import math
+
+def standard_deviation(data):
+    n = len(data)
+    if n <= 1:
+        return 0
+    
+    media = sum(data) / n
+    varianza = sum((x - media) ** 2 for x in data) / (n - 1)
+    return round(math.sqrt(varianza), 3)
