8fb0cb11f0c4f3bc707219284de83379460a02ba
ESTUDIANTE HUANCAYO <estudiantehyo@continental.edu.pe>
2026-01-26 19:18:03 -0500

    Test: caso n=1 (Red)

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,3 +2,7 @@ from src.calculator import standard_deviation
 
 def test_desviacion_vacia():
     assert standard_deviation([]) == 0
+
+
+def test_desviacion_un_dato():
+    assert standard_deviation([5]) == 0
